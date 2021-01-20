19fb217a302f8729ac34aa2a847080f50da7f77c
pedrocontrerasavendano <pedro.contreras.avendano@gmail.com>
2021-01-19 19:07:41 -0500

    Initialise cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,14 @@
 //this will rneder at /cart
-import React from "react"
-
+import React,{useContext} from "react"
+import {Context} from "../context/ContextComponent"
 function Cart() {
+    const {cartItems} = useContext(Context)
+    const cartItemsElelements = cartItems.map(
+        (item)=>(<h2>{item.id}</h2>))
     return (
         <main className="cart-page">
             <h1>Check out!</h1>
+            {cartItemsElelements}
         </main>
     )
 }
