25c63b9be49360be504e390a2d582ab7a8397ed3
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2021-02-19 15:57:00 -0500

    Fix eslint build problems

--- src/context/ContextComponent.js
+++ src/context/ContextComponent.js
@@ -1,52 +1,63 @@
-import React,{useState,useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
 const Context = React.createContext();
 
-function ContextProvider(props){
-    const [error,setError]=useState(null)
-    const [isLoaded,setIsLoaded]=useState(false)
-    const [allPhotos, setAllPhotos] = useState([])
-    const [cartItems, setCartItems] = useState([])
+function ContextProvider(props) {
+  // eslint-disable-next-line
+  const [error, setError] = useState(null);
+  // eslint-disable-next-line
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [allPhotos, setAllPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-    useEffect(()=>{
-        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
-        .then(res=>res.json())
-        .then(
-            (result)=>{
-                setIsLoaded(true)
-                setAllPhotos(result)
-            },
-            (error)=>{
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
-    },[])
-
-    function toggleFavorite(id) {
-        const updatedArr = allPhotos.map(photo => {
-            if(photo.id === id) {
-                return {...photo, isFavorite: !photo.isFavorite}
-            }
-            return photo
-        })
-        setAllPhotos(updatedArr)
-    }
-
-    function addImageToCart(newItem){
-        if(cartItems.find(item=>item.id  === newItem.id)){
-            const result = cartItems.filter((item)=>item.id!==newItem.id)
-            setCartItems(result)
-        }else{
-            setCartItems((prevItems)=>[...prevItems,newItem])
-        } 
-    }
-    
-    return(
-        
-    <Context.Provider value={{allPhotos,cartItems,setCartItems,toggleFavorite,addImageToCart}}>
-        {props.children}
-    </Context.Provider>
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setAllPhotos(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  function toggleFavorite(id) {
+    const updatedArr = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        return { ...photo, isFavorite: !photo.isFavorite };
+      }
+      return photo;
+    });
+    setAllPhotos(updatedArr);
+  }
+
+  function addImageToCart(newItem) {
+    if (cartItems.find((item) => item.id === newItem.id)) {
+      const result = cartItems.filter((item) => item.id !== newItem.id);
+      setCartItems(result);
+    } else {
+      setCartItems((prevItems) => [...prevItems, newItem]);
+    }
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        allPhotos,
+        cartItems,
+        setCartItems,
+        toggleFavorite,
+        addImageToCart,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
 }
-export {ContextProvider, Context}
+export { ContextProvider, Context };
