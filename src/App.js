d0e7515252374e85ec9387c10fc986be6b1d3fab
Pedro Contrears Avendano <pedro.contreras.avendano@gmail.com>
2021-02-20 13:24:07 -0500

    refactor: Updated entry point for application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react"
-import {Switch, Route} from "react-router-dom"
-import Header from "./components/Header.js"
-import Cart from "./pages/Cart.js"
-import Photos from "./pages/Photos.js"
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header.js";
+import Cart from "./pages/Cart.js";
+import Photos from "./pages/Photos.js";
 
-function App() {    
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path="/">
-                    <Photos/>
-                </Route>
-                <Route path="/cart">
-                    <Cart/>
-                </Route>
-            </Switch>
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/react_ecommerce/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-export default App
+export default App;
